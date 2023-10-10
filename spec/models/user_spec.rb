72e8908c4731f611f423bdbbe325a939fee86f98
Favio Valero <favio102@protonmail.com>
2023-10-10 05:36:12 -0700

    display name added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,22 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:tweets).dependent(:destroy) }
   it { should validate_uniqueness_of(:username).case_insensitive.allow_blank }
+
+  describe "set display name" do
+    context "when display name is set" do
+      it "does not change the display name" do
+        user = build(:user, username: "test", display_name: "Test Test")
+        user.save
+        expect(user.reload.display_name).to eq("Test Test")
+      end
+    end
+
+    context "when display name is not set" do
+      it "humanizes the previously set username" do
+        user = build(:user, username: "test", display_name: nil)
+        user.save
+        expect(user.reload.display_name).to eq("Test")
+      end
+    end
+  end
 end
--- spec/requests/usernames_spec.rb
+++ spec/requests/usernames_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe "Usernames", type: :request do
             }
           }
         end.to change { user.reload.username }.from(nil).to("test1")
+        expect(user.display_name).to eq("Test1")
         expect(response).to redirect_to(dashboard_path)
       end
     end
