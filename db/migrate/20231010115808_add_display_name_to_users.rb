babb22158ca0adff814042630d1a34ef124913be
Favio Valero <favio102@protonmail.com>
2023-10-10 17:49:52 -0700

    active storage added

--- db/migrate/20231010115808_add_display_name_to_users.rb
+++ db/migrate/20231010115808_add_display_name_to_users.rb
@@ -2,7 +2,7 @@ class AddDisplayNameToUsers < ActiveRecord::Migration[7.0]
   def up
     add_column :users, :display_name, :string
 
-    query = <<~SQL
+    query = <<~SQL 
       update users
       set display_name = username
       where display_name is null
