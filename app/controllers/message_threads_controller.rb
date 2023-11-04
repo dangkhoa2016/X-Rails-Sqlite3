75a00116f2686db8ebf210db6d9db393d8a6d399
Favio Valero <favio102@protonmail.com>
2023-11-04 00:10:20 -0700

    message thread controller updated , Need to fix user message button

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -1,7 +1,11 @@
+require 'pry'
 class MessageThreadsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
+    if params[:user_id].present?
+      @new_message_thread_user = User.find(params[:user_id])
+    end
+    # binding.pry
   end
 end
