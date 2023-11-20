a4a2f0c6a02533d797a4475d0062b936985bc5db
Favio Valero <favio102@protonmail.com>
2023-11-20 05:09:21 -0800

    creating liked tweet activities

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,5 +9,9 @@ class DashboardController < ApplicationController
     @tweet = Tweet.includes(:liked_users, :bookmarked_users, :retweeted_users, :user).where(user: following_users).order(created_at: :desc).map do |tweet|
       TweetPresenter.new(tweet: tweet, current_user: current_user)
     end
+
+    @tweet_activities = current_user.tweet_activities.order(created_at: :desc).map do |tweet_activity|
+      TweetPresenter.new(tweet: tweet_activity.tweet, current_user: current_user)
+    end
   end
 end
