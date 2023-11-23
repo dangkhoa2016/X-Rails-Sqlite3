4eceb41ba876901720807b391ef87980d889a02b
Favio Valero <favio102@protonmail.com>
2023-11-22 17:37:21 -0800

    fix scroll bug continuation

--- app/javascript/controllers/dashboard_tweets_controller.js
+++ app/javascript/controllers/dashboard_tweets_controller.js
@@ -5,12 +5,11 @@ export default class extends Controller {
   headers = { 'Accept': 'text/vnd.turbo-stream.html' }
 
   connect() {
-    console.log("Hello, Stimulus!");
     window.addEventListener('scroll', () => {
       if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
         if (this.element.dataset.lastPage === 'false') {
           // alert('this is not the last page');
-          fetch(`/?page=${this.element.dataset.nextPage}`, { headers: this.headers }).then(response => response.text()).then(html => Turbo.renderStreamMessage(html));
+          fetch(`dashboard?page=${this.element.dataset.nextPage}`, { headers: this.headers }).then(response => response.text()).then(html => Turbo.renderStreamMessage(html));
         }
       }
     });
